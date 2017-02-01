231c5b3a8c02ccd8bc27685936c5b34f3e5d906f
kostia777999 <kostya.gorozhanov@mail.ru>
2017-02-01 20:58:08 +0200

    fix

--- model/sale/sale-router.js
+++ model/sale/sale-router.js
@@ -5,7 +5,7 @@ const router = new Router();
 let accessToGet = ['admin', 'salesAdmin', 'salesUser'];
 let accessToChange = ['admin', 'salesAdmin'];
 
-router.get('/', auth.hasAnyRole(accessToGet), (...args) => controller.paginate(...args));
+router.get('/', auth.hasAnyRole(accessToGet), (...args) => controller.find(...args));
 router.delete('/:id', auth.hasAnyRole(accessToChange), (...args) => controller.remove(...args));
 router.put('/:id', auth.hasAnyRole(accessToChange), (...args) => controller.update(...args));
 router.get('/:id', auth.hasAnyRole(accessToGet), (...args) => controller.findById(...args));
