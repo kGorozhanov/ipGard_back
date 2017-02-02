27497d6af41d1444879d916154ca136daab1c621
kostia777999 <kostya.gorozhanov@mail.ru>
2017-02-02 21:32:22 +0200

    xxx

--- model/sale/sale-controller.js
+++ model/sale/sale-controller.js
@@ -27,7 +27,7 @@ class SaleController extends Controller {
                 } else if(key === 'date') {
                     let date = new Date(req.query[key]);
                     query[key] = {
-                        $gte: date.getTime(),
+                        $gt: date.getTime(),
                         // $lt: date.setDate(date.getDay() + 1)
                     };
                     console.log(query[key])
