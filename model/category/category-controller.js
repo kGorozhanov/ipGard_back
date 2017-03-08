8d5f2e25e97a8cde7b14e99663f9a14e0e2ace3b
kostia777999 <kostya.gorozhanov@mail.ru>
2017-03-08 13:00:52 +0200

    populate category list

--- model/category/category-controller.js
+++ model/category/category-controller.js
@@ -9,6 +9,8 @@ var async = require('async');
 class CategoryController extends Controller {
     find(req, res, next) {
         return this.model.find(req.query)
+            .then(collection => this.model.populate({path: 'subcategoryField'}))
+            .then(collection => this.model.populate({path: 'relatedCategory'}))
             .then(collection => {
                 let role = req.user.role
                 if (role !== 'admin') {
